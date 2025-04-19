d3906402e3e318d8c7702443fc1f77e0111418a4
Abdelhafid123h <abdelhafidkbirialaoui@gmail.com>
2025-04-19 16:38:32 +0100

    Initialisation du backend Spring Boot

---
+++ stock-management-backend/src/main/java/com/stock_management_backend/StockManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stock_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementBackendApplication.class, args);
+	}
+
+}
---
+++ stock-management-backend/src/test/java/com/stock_management_backend/StockManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
